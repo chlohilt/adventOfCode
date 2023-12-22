80f9cb0dd2a13dc5909c037f1fd2b201e4daaa10
chlohilt <hilton.chloe.y@gmail.com>
2023-12-22 00:30:04 -0700

    finished third day pt 1

--- thirdDay/thirdDay.py
+++ thirdDay/thirdDay.py
@@ -10,7 +10,7 @@ matrix = [[0] * columns for _ in range(rows)]
 file.seek(0)
 for indexLine, line in enumerate(file):
     for index, char in enumerate(line.strip()):
-        matrix[index][indexLine] = char
+        matrix[indexLine][index] = char
 
 
 def checkMatrix(checkPositions):
@@ -24,21 +24,32 @@ def checkMatrix(checkPositions):
             if (matrix[checkPosition[0] - 1][checkPosition[1]] != '.' and
                     not matrix[checkPosition[0] - 1][checkPosition[1]].isdigit()):
                 return True
-        if 0 <= checkPosition[0] + 1 < len(matrix):
-            if (matrix[checkPosition[0] + 1][checkPosition[1] + 1] != '.' and
-                    not matrix[checkPosition[0] + 1][checkPosition[1] + 1].isdigit()):
+            if 0 <= checkPosition[1] + 1 < len(matrix[0]):
+                if (matrix[checkPosition[0] - 1][checkPosition[1] + 1] != '.' and
+                        not matrix[checkPosition[0] - 1][checkPosition[1] + 1].isdigit()):
+                    return True
+
+        if (0 <= checkPosition[1] + 1 < len(matrix[0]) and
+                not matrix[checkPosition[0]][checkPosition[1] + 1].isdigit() and
+                matrix[checkPosition[0]][checkPosition[1] + 1] != '.'):
                 return True
+        if (0 <= checkPosition[1] - 1 < len(matrix[0]) and
+                not matrix[checkPosition[0]][checkPosition[1] - 1].isdigit() and
+                matrix[checkPosition[0]][checkPosition[1] - 1] != '.'):
+                return True
+
+        if 0 <= checkPosition[0] + 1 < len(matrix):
+            if 0 <= checkPosition[1] + 1 < len(matrix[0]):
+                if (matrix[checkPosition[0] + 1][checkPosition[1] + 1] != '.' and
+                        not matrix[checkPosition[0] + 1][checkPosition[1] + 1].isdigit()):
+                    return True
             if (matrix[checkPosition[0] + 1][checkPosition[1]] != '.' and
                     not matrix[checkPosition[0] + 1][checkPosition[1]].isdigit()):
                 return True
-        if 0 <= checkPosition[1] - 1 < len(matrix):
-            if (matrix[checkPosition[0]][checkPosition[1] - 1] != '.' and
-                    not matrix[checkPosition[0] - 1][checkPosition[1] - 1].isdigit()):
-                return True
-        if 0 <= checkPosition[1] + 1 < len(matrix):
-            if (matrix[checkPosition[0]][checkPosition[1] + 1] != '.' and
-                    not matrix[checkPosition[0]][checkPosition[1] + 1].isdigit()):
-                return True
+            if 0 <= checkPosition[1] - 1 < len(matrix[0]):
+                if (matrix[checkPosition[0] + 1][checkPosition[1] - 1] != '.' and # diagonal right lower
+                        not matrix[checkPosition[0] + 1][checkPosition[1] - 1].isdigit()):
+                    return True
 
 # calculate what are numbers and see what is all around them
 file.seek(0)
@@ -50,9 +61,11 @@ for indexLine, line in enumerate(file):
         if char.isdigit():
             currentNumber += char
             checkPositions.append((indexLine, indexChar))
-        else:
+        elif currentNumber != '':
             if checkMatrix(checkPositions):
                 sumOfPartNums += int(currentNumber)
             currentNumber = ''
+            checkPositions.clear()
 
+print(sumOfPartNums)
 file.close()
